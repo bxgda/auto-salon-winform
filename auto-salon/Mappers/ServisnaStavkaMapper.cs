cf5b789bd101626da5b101ae4a767c26143cdc34
David <david.markovic@elfak.rs>
2025-07-28 20:11:16 +0200

    feat(mappers):post PravnoLiceMapper, ServisnaStavkaMapper

---
+++ auto-salon/Mappers/ServisnaStavkaMapper.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using auto_salon.Entities;
+using FluentNHibernate.Mapping;
+
+namespace AutoSalonMac.Mappers
+{
+    public class ServisnaStavkaMapper: ClassMap<ServisnaStavka>
+    {
+        public ServisnaStavkaMapper()
+        {
+            Table("SERVISNA_STAVKA");
+            Id(x => x.ID).Column("ID").GeneratedBy.TriggerIdentity();
+            Map(x => x.Opis).Column("OPIS").Not.Nullable();
+            Map(x => x.Datum).Column("DATUM").Not.Nullable();
+        }
+    }
+}
